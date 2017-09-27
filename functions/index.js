9a7b4b3dc8845c85264a7e72448fefb2e000b5ef
Peter Bengtsson <mail@peterbe.com>
2017-09-26 22:02:01 -0400

    start

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,12 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
+const functions = require('firebase-functions');
 const aws = require('aws-lib')
 import search from './search'
 
-// XXX NEEDS WORK
-const prodAdv = aws.createProdAdvClient(
-  process.env.AWS_ACCESS_KEY_ID,
-  process.env.AWS_SECRET_ACCESS_KEY,
-  process.env.AWS_ASSOCIATE_TAG
-)
-
-admin.initializeApp(functions.config().firebase)
-
-exports.search = functions.https.onRequest((req, res) => {
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.search = functions.https.onRequest((request, response) => {
   console.log('REQ.QUERY', req.query)
   console.log('REQ.BODY', req.body)
-  res.send('Thanks!')
-})
+  response.send("Hello from Firebase!");
+});
